2ca3731c1b7f1aadacb626f5a701066e531d8d36
Rob Goodridge <rob.goodridge@lansa.com.au>
2018-05-30 18:08:52 +1000

    dummy change

--- autodeploy/InitAllLansaevalRepo.ps1
+++ autodeploy/InitAllLansaevalRepo.ps1
@@ -1,6 +1,7 @@
 "InitAllLansaevalRepo.ps1"
 
-For ( $i = 2; $i -le 10; $i++) {
+For ( $i = 101; $i -le 109; $i++) {
+    Write-Output( "lansaeval$($i)")
     git remote add lansaeval$($i) git@github.com:lansa/lansaeval$($i).git
     git remote get-url lansaeval$($i)
     git push --force lansaeval$($i)
