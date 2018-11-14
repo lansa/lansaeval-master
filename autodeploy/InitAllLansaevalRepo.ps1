0f287f5681f302264655b08a3cd09442ecc73b74
Rob Goodridge <rob.goodridge@lansa.com.au>
2018-11-15 10:23:23 +1100

    Setup initial environment

--- autodeploy/InitAllLansaevalRepo.ps1
+++
@@ -1,9 +0,0 @@
-"InitAllLansaevalRepo.ps1"
-
-For ( $i = 310; $i -le 310; $i++) {
-    Write-Output( "lansaeval$($i)")
-    # git remote add lansaeval$($i) git@github.com:lansa/lansaeval$($i).git
-    git remote get-url lansaeval$($i)
-    git push --force lansaeval$($i)
-    Write-Output( "*********************************************")
-}
